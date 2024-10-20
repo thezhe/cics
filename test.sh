8fa137251fabece49150af848e952521c6f73052
Zhe Deng <42720670+thezhe@users.noreply.github.com>
2024-10-19 20:22:45 -0400

    cics upgrade

--- test.sh
+++ test.sh
@@ -28,6 +28,8 @@ readonly test_cics test_script_path
     "${test_cics}" install docker.io/rhysd/actionlint:latest docker.io/koalaman/shellcheck:stable ghcr.io/hadolint/hadolint
     ! "${test_cics}" install ghcr.io/hadolint/hadolint
     ! "${test_cics}" install
+    "${test_cics}" upgrade
+    ! "${test_cics}" upgrade actionlint
     "${test_cics}" list
     ! "${test_cics}" list hadolint
     "${test_cics}" run "${test_script_path}/actionlint" -version
@@ -38,7 +40,13 @@ readonly test_cics test_script_path
     ! "${test_cics}" uninstall actionlint hadolint shellcheck
     ! "${test_cics}" uninstall
     # Post conditions (idempotency)
+    "${test_cics}" upgrade
+    "${test_cics}" list
     ! [ -f "${test_script_path}/actionlint" ]
     ! [ -f "${test_script_path}/hadolint" ]
     ! [ -f "${test_script_path}/shellcheck" ]
+    # cics never removes images
+    podman inspect --type image docker.io/rhysd/actionlint:latest
+    podman inspect --type image docker.io/koalaman/shellcheck:stable
+    podman inspect --type image ghcr.io/hadolint/hadolint
 ) >/dev/null
